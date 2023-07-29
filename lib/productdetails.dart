cd1f36299383f5ee99047d25a5ab824ca7011940
Jan George <janbernaba@hotmail.com>
2023-07-29 18:16:30 +0300

    Product details done

--- lib/productdetails.dart
+++ lib/productdetails.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:racktech/homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class productdetails extends StatelessWidget {
+class productdetails extends StatefulWidget {
   final String image;
   final String name;
   final String description;
 
   productdetails({Key? key, required this.image, required this.name,required this.description})
       : super(key: key);
+
+  @override
+  State<productdetails> createState() => _productdetailsState();
+}
+
+class _productdetailsState extends State<productdetails> {
+  bool showFullDescription = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +43,9 @@ class productdetails extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
+                    borderRadius: BorderRadius.circular(20),
                     image: DecorationImage(
-                      image: NetworkImage(image),
+                      image: NetworkImage(widget.image),
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -47,18 +55,91 @@ class productdetails extends StatelessWidget {
             Container(
               alignment: Alignment.centerLeft,
               margin: const EdgeInsets.all(7.0),
-                child: Text(
-                  name.toUpperCase(),
-                  style: TextStyle(fontSize: 40),
+              child: Text(
+                widget.name.toUpperCase(),
+                style: GoogleFonts.abrilFatface(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.normal,
+                  color: Colors.white,
                 ),
+              ),
             ),
             Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
               margin: const EdgeInsets.all(7.0),
               alignment: Alignment.centerLeft,
+              child: ListTile(
+                title: Text(
+                  "Description",
+                  style: GoogleFonts.abel(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w800,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.white,
+                  ),
+                ),
+                subtitle: showFullDescription
+                    ? Text(
+                  widget.description,
+                  style: GoogleFonts.abel(
+                    textStyle: Theme.of(context).textTheme.headline5,
+                    color: Colors.white,
+                  ),
+                )
+                    : GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showFullDescription = true;
+                    });
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      style: GoogleFonts.abel(
+                        textStyle: Theme.of(context).textTheme.headline5,
+                        color: Colors.white,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: !widget.description.isEmpty
+                              ? widget.description.substring(0, 100)
+                              : "No description",
+                        ),
+                        if (!widget.description.isEmpty)
+                          TextSpan(
+                            text: " \nsee more...", // Show the "see more" part
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue// Making "see more" bolder
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 50,
+              height: 60,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // Set the background color to white
+                  onPrimary: Colors.white, // Set the text color to black
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0), // Set the corner radius
+                  ),
+                ),
+                onPressed: () {},
                 child: Text(
-                  "Description: $description",
+                  'View Datasheet',
                   style: TextStyle(fontSize: 25),
                 ),
+              ),
             ),
           ],
         ),
@@ -66,5 +147,3 @@ class productdetails extends StatelessWidget {
     );
   }
 }
-
-
